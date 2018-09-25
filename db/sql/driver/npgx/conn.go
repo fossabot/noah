5ff23f8929e1a4441ed7c48245ee7db7d6a4cb86
Elliot Courant <elliot.courant@readystock.io>
2018-09-24 19:23:44 -0500

    Adding more functions from PGX.
    I need to figure out a way to return Bytes instead of rows.

--- db/sql/driver/npgx/conn.go
+++ db/sql/driver/npgx/conn.go
@@ -30,10 +30,16 @@
 package npgx
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"github.com/Ready-Stock/Noah/db/sql/driver"
+	"github.com/Ready-Stock/Noah/db/sql/pgio"
 	"github.com/Ready-Stock/Noah/db/sql/pgwire/pgproto"
 	"github.com/Ready-Stock/Noah/db/sql/types"
+	"github.com/Ready-Stock/pgx/pgtype"
 	"github.com/kataras/go-errors"
+	"github.com/kataras/golog"
+	"io"
 	"net"
 	"sync"
 	"time"
@@ -78,6 +84,7 @@ type DialFunc func(network, addr string) (net.Conn, error)
 
 type Conn struct {
 	conn                      net.Conn
+	wbuf					  []byte
 	lastActivityTime          time.Time // the last time the connection was used
 	config                    driver.ConnConfig
 	txStatus                  byte
@@ -167,9 +174,23 @@ func (c *Conn) connect(config driver.ConnConfig, network, address string, dial D
 		}
 
 		switch msg := msg.(type) {
-		case pgproto.BackendKeyData:
-		case pgproto.Authentication:
-		case pgproto.ReadyForQuery:
+		case *pgproto.BackendKeyData:
+		case *pgproto.Authentication:
+			if err = c.rxAuthenticationX(msg); err != nil {
+				return err
+			}
+		case *pgproto.ReadyForQuery:
+			c.rxReadyForQuery(msg)
+			golog.Debugf("connection established to postgres %s", c.config.Host)
+
+			if c.ConnInfo == minimalConnInfo {
+				err = c.initConnInfo()
+				if err != nil {
+					return err
+				}
+			}
+
+			return nil
 		default:
 			if err = c.processContextFreeMsg(msg); err != nil {
 				return err
@@ -186,6 +207,58 @@ func (c *Conn) IsAlive() bool {
 	c.mux.Lock()
 	defer c.mux.Unlock()
 	return c.status >= connStatusIdle
+}
+
+func (c *Conn) initConnInfo() (err error) {
+	var (
+		connInfo *types.ConnInfo
+	)
+
+	if connInfo, err = initPostgresql(c); err == nil {
+		c.ConnInfo = connInfo
+		return err
+	}
+
+	// Check if CrateDB specific approach might still allow us to connect.
+	if connInfo, err = c.crateDBTypesQuery(err); err == nil {
+		c.ConnInfo = connInfo
+	}
+
+	return err
+}
+
+func initPostgresql(c *Conn) (*pgtype.ConnInfo, error) {
+	const (
+		namedOIDQuery = `select t.oid,
+	case when nsp.nspname in ('pg_catalog', 'public') then t.typname
+		else nsp.nspname||'.'||t.typname
+	end
+from pg_type t
+left join pg_type base_type on t.typelem=base_type.oid
+left join pg_namespace nsp on t.typnamespace=nsp.oid
+where (
+	  t.typtype in('b', 'p', 'r', 'e')
+	  and (base_type.oid is null or base_type.typtype in('b', 'p', 'r'))
+	)`
+	)
+
+	nameOIDs, err := connInfoFromRows(c.Query(namedOIDQuery))
+	if err != nil {
+		return nil, err
+	}
+
+	cinfo := pgtype.NewConnInfo()
+	cinfo.InitializeDataTypes(nameOIDs)
+
+	if err = c.initConnInfoEnumArray(cinfo); err != nil {
+		return nil, err
+	}
+
+	if err = c.initConnInfoDomains(cinfo); err != nil {
+		return nil, err
+	}
+
+	return cinfo, nil
 }
 
 func (c *Conn) rxMsg() (pgproto.BackendMessage, error) {
@@ -208,6 +281,41 @@ func (c *Conn) rxMsg() (pgproto.BackendMessage, error) {
 	return msg, nil
 }
 
+func (c *Conn) rxAuthenticationX(msg *pgproto.Authentication) (err error) {
+	switch msg.Type {
+	case pgproto.AuthTypeOk:
+	case pgproto.AuthTypeCleartextPassword:
+		err = c.txPasswordMessage(c.config.Password)
+	case pgproto.AuthTypeMD5Password:
+		digestedPassword := "md5" + hexMD5(hexMD5(c.config.Password+c.config.User)+string(msg.Salt[:]))
+		err = c.txPasswordMessage(digestedPassword)
+	default:
+		err = errors.New("Received unknown authentication message")
+	}
+
+	return
+}
+
+func hexMD5(s string) string {
+	hash := md5.New()
+	io.WriteString(hash, s)
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func (c *Conn) txPasswordMessage(password string) (err error) {
+	buf := c.wbuf
+	buf = append(buf, 'p')
+	sp := len(buf)
+	buf = pgio.AppendInt32(buf, -1)
+	buf = append(buf, password...)
+	buf = append(buf, 0)
+	pgio.SetInt32(buf[sp:], int32(len(buf[sp:])))
+
+	_, err = c.conn.Write(buf)
+
+	return err
+}
+
 func (c *Conn) die(err error) {
 	c.mux.Lock()
 	defer c.mux.Unlock()
@@ -227,15 +335,42 @@ func (c *Conn) processContextFreeMsg(msg pgproto.BackendMessage) (err error) {
 	case *pgproto.ErrorResponse:
 		return c.rxErrorResponse(msg)
 	case *pgproto.NoticeResponse:
-		c.rxNoticeResponse(msg)
+		return errors.New("notice response not supported at this time.")
 	case *pgproto.NotificationResponse:
-		c.rxNotificationResponse(msg)
+		return errors.New("notification response not supported at this time.")
 	case *pgproto.ReadyForQuery:
 		c.rxReadyForQuery(msg)
 	case *pgproto.ParameterStatus:
-		c.rxParameterStatus(msg)
+		return nil
 	}
 
 	return nil
 }
 
+func connInfoFromRows(rows *Rows, err error) (map[string]types.OID, error) {
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	nameOIDs := make(map[string]types.OID, 256)
+	for rows.Next() {
+		var oid types.OID
+		var name types.Text
+		if err = rows.Scan(&oid, &name); err != nil {
+			return nil, err
+		}
+
+		nameOIDs[name.String] = oid
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return nameOIDs, err
+}
+
+func (c *Conn) Query(sql string, args ...interface{}) ([][]byte, error) {
+
+}
