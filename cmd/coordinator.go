7557816448ffdd6458467410515a2d8e0f61e2a0
Elliot Courant <elliot.courant@readystock.io>
2018-12-22 22:44:49 -0600

    Starting to build out cobra for cli.

--- cmd/coordinator.go
+++ cmd/coordinator.go
@@ -18,35 +18,12 @@ var (
 			// Do Stuff Here
 		},
 	}
-
-	join = &cobra.Command{
-		Use:   "join",
-		Short: "Hugo is a very fast static site generator",
-		Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
-		Run: func(cmd *cobra.Command, args []string) {
-
-			// Do Stuff Here
-		},
-	}
-
-	start = &cobra.Command{
-		Use:   "start",
-		Short: "Hugo is a very fast static site generator",
-		Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
-		Run: func(cmd *cobra.Command, args []string) {
-
-			// Do Stuff Here
-		},
-	}
 )
 
 var (
 	PostgresPort  int
 	RaftPort      int
+	JoinAddr      string
 	DataDirectory string
 )
 
@@ -54,7 +31,7 @@ func init() {
 	rootCmd.Flags().IntVar(&PostgresPort, "postgres-port", 5433, "Port that will accept PostgreSQL connections.")
 	rootCmd.Flags().IntVar(&RaftPort, "raft-port", 5434, "Port that will be used for Noah's raft protocol.")
 	rootCmd.Flags().StringVar(&DataDirectory, "data-dir", "data", "Directory that will be used for Noah's key value store.")
-	rootCmd.AddCommand(join, start)
+	rootCmd.Flags().StringVar(&JoinAddr, "join", "", "The address of another node in a cluster to join.")
 }
 
 func Execute() {
