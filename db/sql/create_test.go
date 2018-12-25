508754c9ccc62cee0a69dd69d95365c299f0f7e7
Elliot Courant <elliot.courant@readystock.io>
2018-12-24 23:01:21 -0600

    Starting to build tests for create statements.

--- db/sql/create_test.go
+++ db/sql/create_test.go
@@ -1,1 +1,18 @@
 package sql
+
+import (
+	"github.com/readystock/noah/db/system"
+	"testing"
+)
+
+func Test_Create_CompilePlan(t *testing.T) {
+	stmt := CreateStatement{}
+	plans, err := stmt.compilePlan(nil, make([]system.NNode, 1))
+	if err != nil {
+		panic(err)
+	}
+
+	if len(plans) == 0 {
+		panic("no plans returned")
+	}
+}
