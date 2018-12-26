cdbe207773e53e4b3d9de23b4f8678f2acf1690b
Elliot Courant <elliot.courant@readystock.io>
2018-12-25 21:58:59 -0600

    Added more tests to improve coverage of create statements.

--- db/sql/create_test.go
+++ db/sql/create_test.go
@@ -32,7 +32,7 @@ func TestMain(m *testing.M) {
 }
 
 func Test_Create_CompilePlan_Default(t *testing.T) {
-	sql := `CREATE TABLE users (id bigserial, email text);`
+	sql := `CREATE TABLE abc1 (id bigserial, email text);`
 	parsed, err := pg_query.Parse(sql)
 	if err != nil {
 		panic(err)
@@ -55,12 +55,12 @@ func Test_Create_CompilePlan_Default(t *testing.T) {
 	// This is a simple rewrite, we want to make sure that bigserial is being changed to bigint when
 	// it is found in a create statement. We also want to make sure that the text matches a deparsed
 	// query from pg_query_go.
-	assert.Equal(t, plans[0].CompiledQuery, `CREATE TABLE "users" (id bigint, email text)`,
+	assert.Equal(t, plans[0].CompiledQuery, `CREATE TABLE "abc1" (id bigint, email text)`,
 		"the resulting query plan did not equal the expected query plan, did something change with how queries were recompiled?")
 }
 
 func Test_Create_CompilePlan_AccountNoPrimaryKey(t *testing.T) {
-	sql := `CREATE TABLE users (id bigserial, email text) TABLESPACE "noah.account";`
+	sql := `CREATE TABLE abc2 (id bigserial, email text) TABLESPACE "noah.account";`
 	parsed, err := pg_query.Parse(sql)
 	if err != nil {
 		panic(err)
@@ -73,4 +73,76 @@ func Test_Create_CompilePlan_AccountNoPrimaryKey(t *testing.T) {
 	if err == nil {
 		panic("plan should have failed to compile, account tables require a primary key.")
 	}
+}
+
+func Test_Create_CompilePlan_Account(t *testing.T) {
+	sql := `CREATE TABLE abc3 (id bigserial PRIMARY KEY, email text) TABLESPACE "noah.account";`
+	parsed, err := pg_query.Parse(sql)
+	if err != nil {
+		panic(err)
+	}
+
+	stmt := CreateCreateStatement(parsed.Statements[0].(pg_query2.RawStmt).Stmt.(pg_query2.CreateStmt))
+
+	nodes := make([]system.NNode, 1)
+	plans, err := stmt.compilePlan(ConnExecutor, nodes)
+	if err != nil {
+		panic(err)
+	}
+
+	// For create statements we want to run the query on ALL nodes that we can, so if the number of
+	// nodes that are passed to compile query does not match the number of plans returned then that
+	// means a node is missing or the plans were not generated completely.
+	assert.Equal(t, len(plans), len(nodes),
+		"the number of plans returned did not match the number of nodes that this query should target.")
+
+	// This is a simple rewrite, we want to make sure that bigserial is being changed to bigint when
+	// it is found in a create statement. We also want to make sure that the text matches a deparsed
+	// query from pg_query_go.
+	assert.Equal(t, plans[0].CompiledQuery, `CREATE TABLE "abc3" (id bigint PRIMARY KEY, email text)`,
+		"the resulting query plan did not equal the expected query plan, did something change with how queries were recompiled?")
+}
+
+func Test_Create_CompilePlan_MultiplePrimaryKeys(t *testing.T) {
+	sql := `CREATE TABLE abc4 (id bigserial PRIMARY KEY, email text PRIMARY KEY);`
+	parsed, err := pg_query.Parse(sql)
+	if err != nil {
+		panic(err)
+	}
+
+	stmt := CreateCreateStatement(parsed.Statements[0].(pg_query2.RawStmt).Stmt.(pg_query2.CreateStmt))
+
+	nodes := make([]system.NNode, 1)
+	_, err = stmt.compilePlan(ConnExecutor, nodes)
+	if err == nil {
+		panic("the compiler should not allow you to create a table with multiple primary keys")
+	}
+}
+
+func Test_Create_CompilePlan_ReplacementTypes(t *testing.T) {
+	sql := `CREATE TABLE abc4 (id bigserial, tinyid serial, flake snowflake);`
+	parsed, err := pg_query.Parse(sql)
+	if err != nil {
+		panic(err)
+	}
+
+	stmt := CreateCreateStatement(parsed.Statements[0].(pg_query2.RawStmt).Stmt.(pg_query2.CreateStmt))
+
+	nodes := make([]system.NNode, 1)
+	plans, err := stmt.compilePlan(ConnExecutor, nodes)
+	if err != nil {
+		panic(err)
+	}
+
+	// For create statements we want to run the query on ALL nodes that we can, so if the number of
+	// nodes that are passed to compile query does not match the number of plans returned then that
+	// means a node is missing or the plans were not generated completely.
+	assert.Equal(t, len(plans), len(nodes),
+		"the number of plans returned did not match the number of nodes that this query should target.")
+
+	// This is a simple rewrite, we want to make sure that bigserial is being changed to bigint when
+	// it is found in a create statement. We also want to make sure that the text matches a deparsed
+	// query from pg_query_go.
+	assert.Equal(t, plans[0].CompiledQuery, `CREATE TABLE "abc4" (id bigint, tinyid int, flake bigint)`,
+		"the resulting query plan did not equal the expected query plan, did something change with how queries were recompiled?")
 }
