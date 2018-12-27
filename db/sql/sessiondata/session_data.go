4b420acffead32825020efaadc78f235684de977
Elliot Courant <elliot.courant@readystock.io>
2018-12-27 12:38:45 -0600

    Updated copyright.

--- db/sql/sessiondata/session_data.go
+++ db/sql/sessiondata/session_data.go
@@ -12,51 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
- *
- * This application uses Open Source components. You can find the
- * source code of their open source projects along with license
- * information below. We acknowledge and are grateful to these
- * developers for their contributions to open source.
- *
- * Project: CockroachDB https://github.com/cockroachdb/cockroach
- * Copyright 2018 The Cockroach Authors.
- * License (Apache License 2.0) https://github.com/cockroachdb/cockroach/blob/master/LICENSE
- *
- * Project: Vitess https://github.com/vitessio/vitess
- * Copyright 2018 Google Inc.
- * License (Apache License 2.0) https://github.com/vitessio/vitess/blob/master/LICENSE
- *
- * Project: Citus https://github.com/citusdata/citus
- * Copyright 2018 Citus Data, Inc.
- * License (GNU Affero General Public License v3.0) https://github.com/citusdata/citus/blob/master/LICENSE
- *
- * Project: pg_query_go https://github.com/lfittl/pg_query_go
- * Copyright 2018 Lukas Fittl
- * License (3-Clause BSD) https://github.com/lfittl/pg_query_go/blob/master/LICENSE
- *
- * Project: pgx https://github.com/jackc/pgx
- * Copyright 2018 Jack Christensen
- * License (MIT) https://github.com/jackc/pgx/blob/master/LICENSE
- *
- * Project: BadgerDB https://github.com/dgraph-io/badger
- * Copyright 2018 Dgraph Labs, Inc. and Contributors
- * License (MIT) https://github.com/dgraph-io/badger/blob/master/LICENSE
- *
- * Project: Sonyflake https://github.com/sony/sonyflake
- * Copyright 2018 Sony Corporation
- * License (MIT) https://github.com/sony/sonyflake/blob/master/LICENSE
- *
- * Project: Raft https://github.com/hashicorp/raft
- * Copyright 2018 HashiCorp
- * License (MPL-2.0) https://github.com/hashicorp/raft/blob/master/LICENSE
- *
- * Project: pq github.com/lib/pq
- * Copyright 2018  'pq' Contributors Portions Copyright (C) 2018 Blake Mizerany
- * License https://github.com/lib/pq/blob/master/LICENSE.md
- *
- * Project: go-linq github.com/ahmetb/go-linq
- * Copyright 2018 Ahmet Alp Balkan
- * License https://github.com/ahmetb/go-linq/blob/master/LICENSE
  */
 
 package sessiondata
