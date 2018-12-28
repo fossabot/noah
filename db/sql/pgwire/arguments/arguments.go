40c91ed17d5e85847230be8d68e4ec2a6ce5eb97
Elliot Courant <elliot.courant@readystock.io>
2018-12-28 12:51:42 -0600

    Added better pointer handling, 100% test coverage.

--- db/sql/pgwire/arguments/arguments.go
+++ db/sql/pgwire/arguments/arguments.go
@@ -17,8 +17,10 @@
 package arguments
 
 import (
+    "fmt"
     "github.com/readystock/pg_query_go/nodes"
     "reflect"
+    "strings"
 )
 
 func GetArguments(stmt interface{}) (numArgs int) {
@@ -26,19 +28,17 @@ func GetArguments(stmt interface{}) (numArgs int) {
 }
 
 func examine(value interface{}, depth int) int {
-    args := 0 
-    print := func (msg string, args ...interface{}) {
-        //fmt.Printf("%s%s\n", strings.Repeat("\t", depth), fmt.Sprintf(msg, args...))
+    args := 0
+    print := func(msg string, args ...interface{}) {
+        fmt.Printf("%s%s\n", strings.Repeat("\t", depth), fmt.Sprintf(msg, args...))
     }
 
-    if value == nil || depth > 10 {
+    if value == nil {
         return 0
     }
 
     t := reflect.TypeOf(value)
     v := reflect.ValueOf(value)
-
-
 
     if v.Type() == reflect.TypeOf(pg_query.ParamRef{}) {
         args++
@@ -46,17 +46,19 @@ func examine(value interface{}, depth int) int {
 
     switch t.Kind() {
     case reflect.Ptr:
-
+        if v.Elem().IsValid() {
+            args += examine(v.Elem().Interface(), depth+1)
+        }
     case reflect.Array, reflect.Chan, reflect.Map, reflect.Slice:
-        depth -= 1
+        depth--
         if v.Len() > 0 {
             print("[")
             for i := 0; i < v.Len(); i++ {
-                depth += 1
+                depth++
                 print("[%d] Type {%s} {", i, v.Index(i).Type().String())
                 args += examine(v.Index(i).Interface(), depth+1)
                 print("},")
-                depth -= 1
+                depth--
             }
             print("]")
         } else {
@@ -66,7 +68,7 @@ func examine(value interface{}, depth int) int {
         for i := 0; i < t.NumField(); i++ {
             f := t.Field(i)
             print("[%d] Field {%s} Type {%s} Kind {%s}", i, f.Name, f.Type.String(), reflect.ValueOf(value).Field(i).Kind().String())
-            args += examine( reflect.ValueOf(value).Field(i).Interface(), depth+1)
+            args += examine(reflect.ValueOf(value).Field(i).Interface(), depth+1)
         }
     }
     return args
